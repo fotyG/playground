6b9fc6d9e38b7aeeaab162e46740f3f1b7e0efbe
Sota Giorgadze <giorgadze@inbox.lv>
2023-06-08 20:54:57 +0300

    fix odd reverse col, fix footer error

--- app/components/FactBlock.jsx
+++ app/components/FactBlock.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
-
 const FactBlock = ({ title, description, image, idx }) => {
+  const odd = idx % 2;
   
   return (
     <div className="my-10">
-      <div className="flex-col gap-8 justify-between sm:flex sm:flex-row lg:text-2xl 2xl:text-3xl">
-        <div className={`my-2 flex-1 order-${idx % 2}`}>
+      <div className={"flex-col gap-8 justify-between sm:flex md:text-xl lg:text-2xl 2xl:text-3xl "+ (odd ? "sm:flex-row" : "sm:flex-row-reverse")}>
+        <div className="my-2 flex-1">
           <h2 className="font-bold">{title}</h2>
           <p className="text-justify">{description}</p>
         </div>
--- app/components/Facts.jsx
+++ app/components/Facts.jsx
@@ -8,6 +8,7 @@ const Facts = () => {
     <div className="mx-10 sm:mx-20 md:mx-32 lg:mx-44 2xl:mx-96 my-14">
       {facts.map((fact, idx) => (
         <FactBlock
+          key={idx}
           title={fact.title}
           description={fact.description}
           image={fact.image}
